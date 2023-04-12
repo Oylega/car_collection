edc2a7e7538bdac412be963b66b8cb21f2b99c1b
Oylega <117492599+Oylega@users.noreply.github.com>
2023-04-12 15:54:57 -0400

    Add files via upload

---
+++ app/site/routes.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, jsonify, render_template, request, redirect, url_for
+from helpers import token_required
+from models import db, User, Car, car_schema, cars_schema
+from flask_login import login_required, current_user
+
+site = Blueprint('site', __name__, template_folder='site_templates')
+
+@site.route('/')
+def home():
+    return render_template('index.html')
+
+@site.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html')
+
+@site.route('/cars')
+@login_required
+def cars():
+    cars = Car.query.filter_by(user_token=current_user.token).all()
+    return render_template('cars.html', contacts=contacts)
---
+++ forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm 
+from wtforms import StringField, PasswordField, SubmitField, IntegerField
+from wtforms.validators import DataRequired, Email
+
+class UserLoginForm(FlaskForm):
+    # email, password, submit
+    email = StringField('Email', validators = [DataRequired(), Email()])
+    password = PasswordField('Password', validators = [DataRequired()])
+    submit_button = SubmitField()
+
+class UserSignUpForm(FlaskForm):
+    first = StringField("First Name", validators = [DataRequired()])
+    last = StringField("Last Name", validators = [DataRequired()])
+    email = StringField("Email", validators = [DataRequired(), Email()])
+    password = PasswordField("Password", validators = [DataRequired()])
+    submit_button = SubmitField()
