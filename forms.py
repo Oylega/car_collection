d04da10cb6851509e9f0e8b3a1e1500e690eb587
Oylega <117492599+Oylega@users.noreply.github.com>
2023-04-22 16:45:19 -0400

    Add files via upload

--- forms.py
+++ forms.py
@@ -3,7 +3,6 @@ from wtforms import StringField, PasswordField, SubmitField, IntegerField
 from wtforms.validators import DataRequired, Email
 
 class UserLoginForm(FlaskForm):
-    # email, password, submit
     email = StringField('Email', validators = [DataRequired(), Email()])
     password = PasswordField('Password', validators = [DataRequired()])
     submit_button = SubmitField()
